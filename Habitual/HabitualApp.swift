4ff90eea66dc099ed4b1c7fac86e7feaa0769a80
Morenayd <tofdebby@gmail.com>
2024-07-23 12:17:25 +0100

    Initial Commit

---
+++ Habitual/HabitualApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitualApp.swift
+//  Habitual
+//
+//  Created by Jesutofunmi Adewole on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
